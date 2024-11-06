a2a3f47274236a786a4ba6ec4b9a723be8872675
David Turner GitHub <103603180+davidturner2@users.noreply.github.com>
2024-11-06 06:49:31 -0500

    ASDFGHJKL

--- Assets/walk.cs
+++ Assets/walk.cs
@@ -12,7 +12,6 @@ public class walk : MonoBehaviour
     private Racer trolls;
     private Vector2 mover;
     float accel = 200f;
-    public bool movewithit;
     public void Awake()
     {
         trolls = new Racer();
@@ -50,20 +49,19 @@ public class walk : MonoBehaviour
     void FixedUpdate()
     {
         Vector3 ad = new Vector3(mover.x, 0, mover.y);
-        Vector3 ad2 = new Vector3(0, Camera.main.transform.rotation.eulerAngles.y+90 * mover.x, 0);
-        print(mover.y);
-        if (!movewithit)
-        {
-            child.transform.rotation = Quaternion.RotateTowards(child.transform.rotation, Quaternion.Euler(ad2), accel * Time.deltaTime);
-            r.velocity = Camera.main.transform.rotation * ad * speed;
-        }
-        else
-        {
-            child.transform.rotation = Quaternion.RotateTowards(child.transform.rotation, Quaternion.Euler(child.transform.rotation * ad2), accel * Time.deltaTime);
-
-            r.velocity = child.transform.rotation* ad * speed;
+        Vector3 ad2 = new Vector3(0, Camera.main.transform.rotation.eulerAngles.y+90 * mover.x, 0);;
+        if (mover.y>=0){
+            ad2 = new Vector3(0, Camera.main.transform.rotation.eulerAngles.y+90 * mover.x, 0);
+        }else{
+            ad2 = new Vector3(0, Camera.main.transform.rotation.eulerAngles.y+180 * mover.y, 0);
 
         }
+       // print(mover.y);
+      
+            //child.transform.rotation = Quaternion.RotateTowards(child.transform.rotation, Quaternion.Euler(ad2), accel * Time.deltaTime);
+            r.velocity =child.transform.rotation * ad * speed;
+            
+       
         speed += 0.2f;
         if (speed >= 12)
         {
@@ -82,69 +80,17 @@ public class walk : MonoBehaviour
 
             a.SetBool("walkin", true);
         }
-        /*
-        if (Input.GetKey("w"))
+                RaycastHit down;
+            Vector3 movetothis = child.transform.position+ child.transform.rotation*new Vector3(0,1,0);
+            //Vector3 ra = new Vector3(movetothis.x - 0.3f, movetothis.y + 0.3f, movetothis.z);
+       if (Physics.Raycast(movetothis, child.transform.rotation*child.transform.TransformDirection(Vector3.down), out down, 2f))
         {
-            a.SetBool("walkin", true);
-            //r.AddForce(direction * 20 * boost);
-         
-            r.velocity = child.transform.rotation * (Vector3.forward * speed);
-            if (Input.GetKey("a"))// && !Input.GetKey("w")&& !Input.GetKey("s"))
-            {
-                a.SetBool("walkin", true);
-
-                child.transform.Rotate(new Vector3(0, -4, 0));
-
-                //r.velocity  =Quaternion.Inverse(transform.rotation) *  Vector3.right*20*boost;
-
-            }
-            else if (Input.GetKey("d"))//&& !Input.GetKey("w")&& !Input.GetKey("s"))
-            {
-                a.SetBool("walkin", true);
-
-                child.transform.Rotate(new Vector3(0, 4, 0));
-
-                // r.velocity  =Quaternion.Inverse(transform.rotation) *  Vector3.left*20*boost;
-
-            }
-
+            print(down.collider.gameObject.name);
+            Debug.DrawRay(movetothis, child.transform.rotation*child.transform.TransformDirection(Vector3.down) * down.distance, Color.yellow);
+            child.transform.rotation = Quaternion.FromToRotation(child.transform.up,down.normal)*child.transform.rotation;
+        }else{
+            Debug.DrawRay(movetothis, child.transform.rotation*child.transform.TransformDirection(Vector3.down) * 1f, Color.red);
         }
-        else if (Input.GetKey("a"))// && !Input.GetKey("w")&& !Input.GetKey("s"))
-        {
-            a.SetBool("walkin", true);
-            r.velocity = child.transform.rotation * (Vector3.forward * speed);
-
-            // child.transform.Rotate(new Vector3(0,-1,0));
-
-            //r.velocity  =Quaternion.Inverse(transform.rotation) *  Vector3.right*20*boost;
-
-        }
-        else if (Input.GetKey("d"))//&& !Input.GetKey("w")&& !Input.GetKey("s"))
-        {
-            a.SetBool("walkin", true);
-
-            child.transform.Rotate(new Vector3(0, 4, 0));
-            r.velocity = child.transform.rotation * (Vector3.forward * speed);
-
-            // r.velocity  =Quaternion.Inverse(transform.rotation) *  Vector3.left*20*boost;
-
-        }
-
-        else if (Input.GetKey("s"))
-        {
-            a.SetBool("walkin", true);
-
-
-            r.velocity = child.transform.rotation * (Vector3.back * 8);
-
-        }
-        else
-        {
-            a.SetBool("walkin", false);
-            speed = 0;
-
-        }
-        */
 
     }
     void Jump()
@@ -170,4 +116,5 @@ public class walk : MonoBehaviour
     {
         
     }
+    
 }
